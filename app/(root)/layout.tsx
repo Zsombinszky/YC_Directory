4da7f03b5010d7bba619ce464fbb56d5fe6a645e
Zsombinszky <zsombi.studio@gmail.com>
2024-11-04 19:06:55 +0100

    startup creation and authentication implemented

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Navbar from "@/components/Navbar";
+
+export default function Layout({children}: Readonly<{ children: React.ReactNode }>) {
+    return (
+        <div className="font-work-sans">
+            <Navbar/>
+            {children}
+        </div>
+    )
+}
---
+++ lib/utils.ts
@@ -0,0 +1,22 @@
+import {clsx, type ClassValue} from "clsx"
+import {twMerge} from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+    return twMerge(clsx(inputs))
+}
+
+export function formatDate(date: string) {
+    return new Date(date).toLocaleDateString('en-US', {
+        month: 'long',
+        day: 'numeric',
+        year: 'numeric'
+    })
+}
+
+export function formatNumber(number: number) {
+    return number > 9 ? `Views: ${number}` : `View: ${number}`
+}
+
+export function parseServerActionResponse<T>(response: T) {
+    return JSON.parse(JSON.stringify(response))
+}
